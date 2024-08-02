7fa9b831c4e319bc02a0c4c4b17dc24f1d552fe6
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-08-02 14:22:40 +0300

    formated price

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,21 +1,23 @@
-export default function MealItem({meal}) {
-    return (
-        <>
-        <li className="meal-item">
-            <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt="" />
-            <div>
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">{meal.price}</p>
-            <p className="meal-item-description">{meal.description}</p>
-            </div>
-            <p className="meal-item-actions">
-            <button>Add to cart</button>
-            </p>
-            
-            </article>
-        </li>
-        </>
-    )
+import { formatedPrice } from "../format";
 
+export default function MealItem({ meal }) {
+  return (
+    <>
+      <li className="meal-item">
+        <article>
+          <img src={`http://localhost:3000/${meal.image}`} alt="" />
+          <div>
+            <h3>{meal.name}</h3>
+            <p className="meal-item-price">
+              {formatedPrice.format(meal.price)}
+            </p>
+            <p className="meal-item-description">{meal.description}</p>
+          </div>
+          <p className="meal-item-actions">
+            <button>Add to cart</button>
+          </p>
+        </article>
+      </li>
+    </>
+  );
 }
---
+++ src/format.js
@@ -0,0 +1,5 @@
+export const formatedPrice =  new Intl.NumberFormat(`en-US` , {
+    style : 'currency',
+    currency : 'USD'
+
+})
