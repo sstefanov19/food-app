74f06a5ead75a53a5ecf62edb845aa8e43f50863
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-08-02 16:31:13 +0300

    updating number of items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react'
 import logo from '../assets/logo.jpg'
 import Button from './Button'
+import CartContext from '../store/CartContext'
 export default function Header() {
+
+    const cartCtx = useContext(CartContext)
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems , item) => { 
+        return totalNumberOfItems + item.quantity
+    } , 0);
+
     return (
         <header id="main-header">
             <div id='title'>
@@ -8,7 +17,7 @@ export default function Header() {
                 <h1>React food</h1>
             </div>
             <nav>
-                <Button textOnly>Cart (0)</Button>
+                <Button textOnly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
